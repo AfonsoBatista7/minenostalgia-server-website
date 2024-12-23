6f6a2d9399a51d1ce8cc0710a8ea3245092b9042
AfonsoBatista07 <afonsobatista13@gmail.com>
2024-12-23 13:23:48 +0000

    [Fix] - backend connecting to local mongo

--- source/MongoDB/data/populateDb.sh
+++ source/MongoDB/data/populateDb.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo "Populating MongoDB..."
-
 # Assigning arguments to variables
 MONGO_INITDB_ROOT_USERNAME="$1"
 MONGO_INITDB_ROOT_PASSWORD="$2"
@@ -10,21 +8,20 @@ MONGO_PORT="$4"
 DUMP_DIR="$5"
 DB_NAME="DiscordBot"
 
-echo "mongodb://$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@$MONGO_HOST:$MONGO_PORT/$DB_NAME"
-echo $DUMP_DIR
-
 # Restore MongoDB database from dump
+echo "Populating MongoDB..."
 mongorestore --uri "mongodb://$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@$MONGO_HOST:$MONGO_PORT" --nsInclude="${DB_NAME}.*" --drop "$DUMP_DIR"
 
 # Create MongoDB user
-echo "Creating MongoDB user..."
-mongosh --host "$MONGO_HOST" --port "$MONGO_PORT" -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase admin --eval "
+echo "Creating MongoDB user $MONGO_INITDB_ROOT_USERNAME..."
+mongosh --host "$MONGO_HOST" --port "$MONGO_PORT" \
+    -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase admin <<EOF
 use $DB_NAME;
 db.createUser({
-    user: '$MONGO_INITDB_ROOT_USERNAME',
-    pwd: '$MONGO_INITDB_ROOT_PASSWORD',
+    user: "$MONGO_INITDB_ROOT_USERNAME",
+    pwd: "$MONGO_INITDB_ROOT_PASSWORD",
     roles: [{ role: 'readWrite', db: '$DB_NAME' }]
 });
-"
+EOF
 
 echo "User created and database populated successfully."
